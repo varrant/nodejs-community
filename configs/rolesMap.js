fd42f6a6b4c07d9b420c0767c91af0ec4b5804bf
cloudcome <cloudcome@163.com>
2014-12-02 00:06:14 +0800

    doc & settings

--- configs/rolesMap.js
+++ configs/rolesMap.js
@@ -15,61 +15,6 @@ module.exports = function (app) {
         desc: '发布观点'
     };
 
-    rolesMap[_pow2(number++)] = {
-        name: 'edit post',
-        desc: '编辑观点'
-    };
-
-    rolesMap[_pow2(number++)] = {
-        name: 'remove post',
-        desc: '删除观点'
-    };
-
-    rolesMap[_pow2(number++)] = {
-        name: 'create note',
-        desc: '撰写笔记'
-    };
-
-    rolesMap[_pow2(number++)] = {
-        name: 'edit note',
-        desc: '编辑笔记'
-    };
-
-    rolesMap[_pow2(number++)] = {
-        name: 'remove note',
-        desc: '删除笔记'
-    };
-
-    rolesMap[_pow2(number++)] = {
-        name: 'create note',
-        desc: '发布提问'
-    };
-
-    rolesMap[_pow2(number++)] = {
-        name: 'edit note',
-        desc: '编辑提问'
-    };
-
-    rolesMap[_pow2(number++)] = {
-        name: 'remove note',
-        desc: '删除提问'
-    };
-
-    rolesMap[_pow2(number++)] = {
-        name: 'create comment',
-        desc: '发布提问'
-    };
-
-    rolesMap[_pow2(number++)] = {
-        name: 'edit comment',
-        desc: '编辑提问'
-    };
-
-    rolesMap[_pow2(number++)] = {
-        name: 'remove comment',
-        desc: '删除提问'
-    };
-
     return rolesMap;
 };
 
--- docs/interactive.md
+++ docs/interactive.md
@@ -1,8 +1,12 @@
 # interactive
+
+如被点赞、被评论、被收藏、申请加入组织等
+
 - `model` 模型
 - `path` 字段
 - `object` 对象，即ID
 - `operator` 操作者
+- `value` 影响值，默认1
 - `at` 操作时间
 - `isApprove` 是否被允许
 - `meta` 元信息
