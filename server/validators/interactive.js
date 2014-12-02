f6b8bf04e71f0942bef32069ed966ce15d1c0b94
cloudcome <cloudcome@163.com>
2014-12-02 22:31:35 +0800

    validator interactive

---
+++ server/validators/interactive.js
@@ -0,0 +1,13 @@
+/*!
+ * interactive 验证规则
+ * @author ydr.me
+ * @create 2014-12-02 22:29
+ */
+
+'use strict';
+
+
+var Validator = require('ydr-validator');
+var validator = new Validator();
+
+module.exports = validator;
