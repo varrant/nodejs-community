8c2fc6556a4f892c09564bc82e55808d216d8589
cloudcome <cloudcome@163.com>
2014-12-02 22:31:25 +0800

    validator comment

---
+++ server/validators/comment.js
@@ -0,0 +1,13 @@
+/*!
+ * comment 验证规则
+ * @author ydr.me
+ * @create 2014-12-02 22:28
+ */
+
+'use strict';
+
+
+var Validator = require('ydr-validator');
+var validator = new Validator();
+
+module.exports = validator;
