8de70274cb987bdc946cafbbf748a4c0a13ca13f
cloudcome <cloudcome@163.com>
2014-12-02 22:32:01 +0800

    validator scope

---
+++ server/validators/scope.js
@@ -0,0 +1,13 @@
+/*!
+ * scope 验证规则
+ * @author ydr.me
+ * @create 2014-12-02 22:30
+ */
+
+'use strict';
+
+
+var Validator = require('ydr-validator');
+var validator = new Validator();
+
+module.exports = validator;
