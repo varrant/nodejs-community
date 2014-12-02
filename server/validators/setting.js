3f6284d3335851894fc1a26095e1ecf22625b5d6
cloudcome <cloudcome@163.com>
2014-12-02 22:32:09 +0800

    validator setting

--- server/validators/setting.js
+++ server/validators/setting.js
@@ -1,5 +1,5 @@
 /*!
- * 文件描述
+ * setting 验证规则
  * @author ydr.me
  * @create 2014-11-22 20:42
  */
@@ -8,13 +8,5 @@
 
 var Validator = require('ydr-validator');
 var validator = new Validator();
-
-validator.pushRule({
-    name: 'key',
-    alias: '配置名称',
-    type: 'string',
-    required: true,
-    trim: true
-});
 
 module.exports = validator;
