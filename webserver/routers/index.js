072711a6c77c18da0d24c36aacd7e8191620732e
cloudcome <cloudcome@163.com>
2014-12-04 16:26:49 +0800

    router

--- webserver/routers/index.js
+++ webserver/routers/index.js
@@ -26,13 +26,28 @@ log.setOptions('path', configs.dir.log);
 module.exports = function (app) {
     var exports = controllers(app);
 
+    // 中间件：路由验证、安全验证、访问验证等
     require('./middleware.js')(app, exports.middleware);
+
+    // 静态路由
     app.use('/', express.static(configs.dir.webroot, staticOptions));
     app.use('/static/', express.static(configs.dir.static, staticOptions));
+
+    // 测试
     require('./test.js')(app, exports.test);
+
+    // 管理路由
     require('./admin.js')(app, exports.admin);
+
+    // 前端路由
     require('./front.js')(app, exports.front);
+
+    // API 路由
     require('./api.js')(app, exports.api);
+
+    // notFound/serverError 日志
     app.use(log());
+
+    // 终点路由
     require('./error.js')(app, exports.error);
 };
