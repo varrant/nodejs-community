a5a3f40d2406d3b3e527d93baccce7afade54ff0
cloudcome <cloudcome@163.com>
2014-12-04 17:05:12 +0800

    template

--- webserver/controllers/front/main.js
+++ webserver/controllers/front/main.js
@@ -1,5 +1,5 @@
 /*!
- * 文件描述
+ * 主
  * @author ydr.me
  * @create 2014-12-03 21:19
  */
@@ -10,7 +10,7 @@ module.exports = function (app) {
     var exports = {};
 
     exports.home = function(req, res, next){
-        res.render('frontend/home.html');
+        res.render('front/home.html');
     };
 
     return exports;
