aac17d53408f4f33024fe65567cd6441c45407f0
cloudcome <cloudcome@163.com>
2014-12-08 23:52:47 +0800

    views

--- webserver/controllers/front/main.js
+++ webserver/controllers/front/main.js
@@ -7,6 +7,8 @@
 'use strict';
 
 var random = require('ydr-util').random;
+var dato = require('ydr-util').dato;
+var typeis = require('ydr-util').typeis;
 
 module.exports = function (app) {
     var exports = {};
@@ -24,47 +26,47 @@ module.exports = function (app) {
                 viewCount: random.number(20, 10000),
                 commentCount: random.number(20, 10000),
                 favoriteCount: random.number(20, 10000)
-            },{
+            }, {
                 title: 'metro ui 侧边栏问题，使用框架后，单击左侧按钮好，怎样使左侧的菜单处于原来的状态？',
                 viewCount: random.number(20, 10000),
                 commentCount: random.number(20, 10000),
                 favoriteCount: random.number(20, 10000)
-            },{
+            }, {
                 title: '缩略图样式里面的代码是什么意思？其中一小段',
                 viewCount: random.number(20, 10000),
                 commentCount: random.number(20, 10000),
                 favoriteCount: random.number(20, 10000)
-            },{
+            }, {
                 title: 'chart.js 怎么做响应式？',
                 viewCount: random.number(20, 10000),
                 commentCount: random.number(20, 10000),
                 favoriteCount: random.number(20, 10000)
-            },{
+            }, {
                 title: '组合输入框使用Glyphicon在一个手机上显示正常，另外一个手机显示 这样[图]',
                 viewCount: random.number(20, 10000),
                 commentCount: random.number(20, 10000),
                 favoriteCount: random.number(20, 10000)
-            },{
+            }, {
                 title: '在不联网的情况下，怎么使用Glyphicons 字体图标呢？',
                 viewCount: random.number(20, 10000),
                 commentCount: random.number(20, 10000),
                 favoriteCount: random.number(20, 10000)
-            },{
+            }, {
                 title: '如何让面板是直角展示呢！',
                 viewCount: random.number(20, 10000),
                 commentCount: random.number(20, 10000),
                 favoriteCount: random.number(20, 10000)
-            },{
+            }, {
                 title: '表单构造器中拖拽的表单都是中间对齐的,但为什么拷下来源码就不是这样显示?',
                 viewCount: random.number(20, 10000),
                 commentCount: random.number(20, 10000),
                 favoriteCount: random.number(20, 10000)
-            },{
+            }, {
                 title: 'where！',
                 viewCount: random.number(20, 10000),
                 commentCount: random.number(20, 10000),
                 favoriteCount: random.number(20, 10000)
-            },{
+            }, {
                 title: '【请教】BootStrap的JavaScript插件问题',
                 viewCount: random.number(20, 10000),
                 commentCount: random.number(20, 10000),
@@ -75,18 +77,50 @@ module.exports = function (app) {
 
 
     /**
-     * post 页
+     * list 页
      * @param req
-     * @param res
-     * @param next
+     * @param type
+     * @returns {Function}(req, res, next)
      */
-    exports.getPost = function (req, res, next) {
-        var type = req.params[0];
-        var uri = req.params[1];
+    exports.getList = function (type) {
+        return function (req, res, next) {
+            var scope = req.params.scope;
+            var label = req.params.label;
+            var page = req.params.page;
 
-        res.render('front/post-' + type + '.html', {
-            list: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        });
+            page = dato.parseInt(page, 1);
+
+            res.json({
+                type: type,
+                scope: scope,
+                label: label,
+                page: page
+            });
+        };
+    };
+
+
+    /**
+     * post 页
+     * @param type
+     * @returns {Function}(req, res, next)
+     */
+    exports.getPost = function (type) {
+        return function (req, res, next) {
+            var uri = req.params.uri;
+
+            res.render('front/post-' + type + '.html', {
+                list: [{
+                    content: '清除浏览器缓存的文件试试看！不知道有没有童鞋遇到过这样的？我用QQ,搜狗，猎豹，chrome，UC都没问题！！手机wifi调试页面，浏览器是小米3内置浏览器童鞋遇到过这样的？我用QQ,搜狗，猎豹，chrome，UC都没问题！！手机wifi调试'
+                }, {
+                    content: '我本地wamp环境，手机wifi调试页面，浏览器是小米3内置浏览器；'
+                }, {
+                    content: '那么问题是：css文件修改后，浏览器不管怎么刷新，（手机，浏览器）重启都不能呈现更新后的样式。'
+                }, {
+                    content: '不知道有没有童鞋遇到过这样的？我用QQ,搜狗，猎豹，chrome，UC都没问题！！小米浏览器是我见过的手机浏览器最垃圾的 号召大家都不要睬他'
+                }]
+            });
+        }
     };
 
     return exports;
--- webserver/routers/front.js
+++ webserver/routers/front.js
@@ -7,17 +7,15 @@
 'use strict';
 
 
-module.exports = function(app, ctrlFront){
+module.exports = function (app, ctrlFront) {
     var types = app.locals.options.types;
     var names = [];
 
     types.forEach(function (type) {
-        if(names.indexOf(type.name) === -1 && type.isDisplay){
+        if (names.indexOf(type.name) === -1 && type.isDisplay) {
             names.push(type.name);
         }
     });
-
-    var regPost = new RegExp('^\\/('+names.join('|')+')\\/([\\w-.]+)\\.html$');
 
     // user
     app.get('/user/oauth/authorize/', ctrlFront.user.oauthAuthorize);
@@ -25,5 +23,16 @@ module.exports = function(app, ctrlFront){
 
     // home
     app.get('/', ctrlFront.main.getHome);
-    app.get(regPost, ctrlFront.main.getPost);
+    names.forEach(function (name) {
+        app.get('/' + name + '/', ctrlFront.main.getList(name));
+        app.get('/' + name + '/' + 'page/:page/', ctrlFront.main.getList(name));
+        app.get('/' + name + '/' + 'in/:scope/', ctrlFront.main.getList(name));
+        app.get('/' + name + '/' + 'in/:scope/page/:page/', ctrlFront.main.getList(name));
+        app.get('/' + name + '/' + 'on/:label/page/:page/', ctrlFront.main.getList(name));
+        app.get('/' + name + '/' + 'in/:scope/on/:label/', ctrlFront.main.getList(name));
+        app.get('/' + name + '/' + 'in/:scope/on/:label/page/:page/', ctrlFront.main.getList(name));
+        app.get('/' + name + '/' + 'on/:label/in/:scope/', ctrlFront.main.getList(name));
+        app.get('/' + name + '/' + 'on/:label/in/:scope/page/:page/', ctrlFront.main.getList(name));
+        app.get('/' + name + '/' + ':uri.html', ctrlFront.main.getPost(name));
+    });
 };
