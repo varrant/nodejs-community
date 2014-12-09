0687cd48169ac08563f05595d4bb7feacc5da829
cloudcome <cloudcome@163.com>
2014-12-09 15:17:42 +0800

    webserver/index

---
+++ webserver/index.js
@@ -0,0 +1,32 @@
+/*!
+ * webserver 入口
+ * @author ydr.me
+ * @create 2014-12-09 15:08
+ */
+
+'use strict';
+
+var howdo = require('howdo');
+var configs = require('../configs/');
+var mongoose = require('./mongoose.js');
+var express = require('./express.js');
+var middleware = require('./middleware.js');
+var http = require('./http.js');
+
+howdo
+    .task(mongoose)
+    .task(express)
+    .task(middleware)
+    .task(http)
+    .follow(function (err, app) {
+        if (err) {
+            console.log(err);
+            return process.exit(-1);
+        }
+
+        console.log('');
+        console.log('#########################################################');
+        console.log(configs.app.domain + ' running at ' + app.locals.settings.port);
+        console.log('#########################################################');
+        console.log('');
+    });
