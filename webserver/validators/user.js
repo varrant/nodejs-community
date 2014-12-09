f8f95c1b65068742d375913b6c2b7370dea74cfa
cloudcome <cloudcome@163.com>
2014-12-09 15:23:15 +0800

    validator user

--- webserver/validators/user.js
+++ webserver/validators/user.js
@@ -9,32 +9,4 @@
 var Validator = require('ydr-validator');
 var validator = new Validator();
 
-validator.pushRule({
-    name: 'email',
-    alias: '邮箱帐号',
-    type: 'email',
-    required: true,
-    trim: true
-});
-
-validator.pushRule({
-    name: 'github',
-    alias: 'github 帐号',
-    type: 'string',
-    required: true,
-    trim: true,
-    minLength: 1,
-    maxLength: 100
-});
-
-validator.pushRule({
-    name: 'nickname',
-    alias: '昵称',
-    type: 'string',
-    required: true,
-    trim: true,
-    minLength: 2,
-    maxLength: 12
-});
-
 module.exports = validator;
