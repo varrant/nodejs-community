8f7a5680465e016792c9879e63e10239632d1834
cloudcome <cloudcome@163.com>
2014-12-09 15:38:07 +0800

    model

--- webserver/validators/user.js
+++ webserver/validators/user.js
@@ -9,4 +9,23 @@
 var Validator = require('ydr-validator');
 var validator = new Validator();
 
+validator.pushRule({
+    name: 'email',
+    alias: '邮箱帐号',
+    type: 'email',
+    required: true,
+    maxLength: 256,
+    trim: true
+});
+
+validator.pushRule({
+    name: 'nickname',
+    alias: '昵称',
+    type: 'string',
+    required: true,
+    trim: true,
+    minLength: 2,
+    maxLength: 12
+});
+
 module.exports = validator;
