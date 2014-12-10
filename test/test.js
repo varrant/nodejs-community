adedec5594efdd14f82a9efdb8c86bde0d5c63cc
cloudcome <cloudcome@163.com>
2014-12-10 13:25:11 +0800

    test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 /*!
- * 文件描述
+ * model test
  * @author ydr.me
  * @create 2014-11-24 20:41
  */
@@ -23,147 +23,147 @@ mongoose(function (err) {
     var t1 = Date.now();
 
     howdo
-        //.task(function (next) {
-        //    console.log('\n=========== test createOne ===========');
-        //
-        //    user.createOne({
-        //        email: random.string(10) + '@domain.com',
-        //        github: random.string(10),
-        //        nickname: random.string(10)
-        //    }, function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test existOne ===========');
-        //
-        //    user.existOne({
-        //        github: 'abcdefg'
-        //    },{
-        //        email: 'abcdefg@domain.com',
-        //        nickname: '呵呵'
-        //    }, function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test findOneAndUpdate ===========');
-        //
-        //    user.findOneAndUpdate({
-        //        github: 'abcdefg'
-        //    },{
-        //        email: 'abcdefghhhhhhhhhhhhhhhh@domain.com',
-        //        nickname: '呵呵dddddddd'
-        //    }, function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test getMeta ===========');
-        //
-        //    user.getMeta({
-        //        github: 'cloudcome'
-        //    }, 'bio', function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test setMeta ===========');
-        //
-        //    user.setMeta({
-        //        github: 'cloudcome'
-        //    }, 'bio', '呵呵', function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test setMeta ===========');
-        //
-        //    user.setMeta({
-        //        github: 'cloudcome'
-        //    }, {
-        //        bio: '呵呵呵呵'
-        //    }, function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test increase ===========');
-        //
-        //    user.increase({
-        //        github: 'cloudcome'
-        //    }, 'followCount', 1, function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test push ===========');
-        //
-        //    var id = random.string(24, 'a0');
-        //    console.log(id);
-        //    user.push({
-        //        github: 'cloudcome'
-        //    }, 'organizations', id, function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test push ===========');
-        //
-        //    var id = '001300000004ad0009000000';
-        //    console.log(id);
-        //    user.pull({
-        //        github: 'cloudcome'
-        //    }, 'organizations', id, function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test toggle ===========');
-        //
-        //    user.toggle({
-        //        github: 'cloudcome'
-        //    }, 'isBlock', true, function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test find ===========');
-        //
-        //    user.find({}, {limit: 2}, function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test findOne ===========');
-        //
-        //    user.findOne({}, function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.task(function (next) {
-        //    console.log('\n=========== test count ===========');
-        //
-        //    user.count({}, function () {
-        //        console.log(arguments);
-        //        next();
-        //    });
-        //})
-        //.follow(function () {
-        //    var t = Date.now() - t1;
-        //    console.log('\n=========== test end (' + t + 'ms) ===========');
-        //    process.exit();
-        //});
+        .task(function (next) {
+            console.log('\n=========== test createOne ===========');
+
+            user.createOne({
+                email: random.string(10) + '@domain.com',
+                github: random.string(10),
+                nickname: random.string(10)
+            }, function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test existOne ===========');
+
+            user.existOne({
+                github: 'abcdefg'
+            },{
+                email: 'abcdefg@domain.com',
+                nickname: '呵呵'
+            }, function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test findOneAndUpdate ===========');
+
+            user.findOneAndUpdate({
+                github: 'abcdefg'
+            },{
+                email: 'abcdefghhhhhhhhhhhhhhhh@domain.com',
+                nickname: '呵呵dddddddd'
+            }, function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test getMeta ===========');
+
+            user.getMeta({
+                github: 'cloudcome'
+            }, 'bio', function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test setMeta ===========');
+
+            user.setMeta({
+                github: 'cloudcome'
+            }, 'bio', '呵呵', function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test setMeta ===========');
+
+            user.setMeta({
+                github: 'cloudcome'
+            }, {
+                bio: '呵呵呵呵'
+            }, function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test increase ===========');
+
+            user.increase({
+                github: 'cloudcome'
+            }, 'followCount', 1, function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test push ===========');
+
+            var id = random.string(24, 'a0');
+            console.log(id);
+            user.push({
+                github: 'cloudcome'
+            }, 'organizations', id, function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test push ===========');
+
+            var id = '001300000004ad0009000000';
+            console.log(id);
+            user.pull({
+                github: 'cloudcome'
+            }, 'organizations', id, function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test toggle ===========');
+
+            user.toggle({
+                github: 'cloudcome'
+            }, 'isBlock', true, function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test find ===========');
+
+            user.find({}, {limit: 2}, function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test findOne ===========');
+
+            user.findOne({}, function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .task(function (next) {
+            console.log('\n=========== test count ===========');
+
+            user.count({}, function () {
+                console.log(arguments);
+                next();
+            });
+        })
+        .follow(function () {
+            var t = Date.now() - t1;
+            console.log('\n=========== test end (' + t + 'ms) ===========');
+            process.exit();
+        });
 });
