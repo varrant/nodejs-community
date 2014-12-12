beb0eb5e6e22aec413e865309c8b7fa49f9bf470
cloudcome <cloudcome@163.com>
2014-12-12 13:08:17 +0800

    alpha5

--- webserver/validators/scope.js
+++ webserver/validators/scope.js
@@ -9,5 +9,24 @@
 
 var Validator = require('ydr-validator');
 var validator = new Validator();
+var REG_LINES = /[\n\s]{2,}/g;
+
+validator.pushRule({
+    name: 'cover',
+    alias: '封面',
+    type: 'string',
+    minLength: 10,
+    maxLength: 255
+});
+
+validator.pushRule({
+    name: 'introduction',
+    alias: '介绍',
+    type: 'string',
+    maxLength: 1000,
+    onafter: function (val) {
+        return val.replace(REG_LINES, '\n\n');
+    }
+});
 
 module.exports = validator;
