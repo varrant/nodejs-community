07b5f926e07b0a47acca86c2616b96402ed33b51
cloudcome <cloudcome@163.com>
2014-12-13 18:51:42 +0800

    alpha6

--- webserver/services/notification.js
+++ webserver/services/notification.js
@@ -1,5 +1,5 @@
 /*!
- * 文件描述
+ * notification service
  * @author ydr.me
  * @create 2014-12-12 18:15
  */
@@ -7,35 +7,13 @@
 'use strict';
 
 var notification = require('../models/').notification;
-var configs = require('../../configs/');
 var email = require('./email.js');
-var from = configs.smtp.from;
 var log = require('ydr-util').log;
-var noticonfig = configs.notification;
 
 /**
  * 创建一条通知
- * @param activeUser
- * @param activedUser
- * @param bject
  */
-exports.createOne = function (type, activeUser, activedUser, object) {
-    var data = {
-        type: type,
-        activeUser: activedUser.id,
-        activedUser: activedUser.id,
-        obejct: object.id
-    };
-    var notitemplate = noticonfig[type];
-    var subject = notitemplate.subject;
-    var content = notitemplate.template.render({});
-
-    // 创建通知
-    notification.createOne(data, log.holdError);
-    // 发送邮件
-    email.send(activedUser.nickname, activedUser.email, subject, content);
-};
-
+exports.createOne = notification.createOne;
 
 /**
  * 设置为已激活
