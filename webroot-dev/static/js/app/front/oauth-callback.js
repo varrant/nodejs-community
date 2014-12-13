9d606630814ae5b96736d8a2f2f6600582080263
cloudcome <cloudcome@163.com>
2014-12-13 22:23:26 +0800

    alpha6

---
+++ webroot-dev/static/js/app/front/oauth-callback.js
@@ -0,0 +1,16 @@
+/*!
+ * 授权登录回调
+ * @author ydr.me
+ * @create 2014-12-13 22:20
+ */
+
+
+define(function (require, exports, module) {
+    'use strict';
+
+    var xhr = require('../../alien/core/communication/xhr.js');
+    var selector = require('../../alien/core/dom/selector.js');
+    var event = require('../../alien/core/event/base.js');
+
+    module.exports = {};
+});
