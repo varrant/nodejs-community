edf66cbfabf1f32019cbe2ec0c248706ea968fe8
cloudcome <cloudcome@163.com>
2014-12-14 14:33:44 +0800

    alpha7

--- docs/readme.md
+++ docs/readme.md
@@ -1,14 +1,13 @@
 # 功能
 - app.locals.$smtp smtp 服务器
-- app.locals.$options 网站配置
-- app.locals.$admin 网站管理员
+- app.locals.$options 社区配置
 - req.session.$csrf 请求令牌
 - req.session.$user 请求用户
 - req.session.$github github授权
 - req.session.$state 授权状态值
 - res.locals.$csrf 响应令牌
 - res.locals.$user 响应用户
-- res.locals.$types 响应板块
+- res.locals.$options 响应社区配置
 
 
 # 参考项目
--- webserver/middleware.js
+++ webserver/middleware.js
@@ -6,7 +6,6 @@
 
 'use strict';
 
-var ydrUtil = require('ydr-util');
 var setting = require('./services/').setting;
 var user = require('./services/').user;
 var howdo = require('howdo');
@@ -20,35 +19,33 @@ module.exports = function (next, app) {
                     return done(err);
                 }
 
-                ydrUtil.dato.extend(true, app.locals, {
-                    $options: settings
-                });
+                app.locals.$options = settings;
 
                 done();
             });
         })
-        // 初始化网站管理员
-        .task(function (done) {
-            user.findOne({
-                role: 2097151
-            }, function (err, doc) {
-                if(err){
-                    return done(err);
-                }
-
-                if(!doc){
-                    console.log('');
-                    console.log('#########################################################');
-                    console.log('miss a owner, please read readme.md and use `npm install`');
-                    console.log('#########################################################');
-                    console.log('');
-                    return process.exit();
-                }
-
-                app.locals.$admin = doc.toObject();
-                done();
-            });
-        })
+        //// 初始化网站管理员
+        //.task(function (done) {
+        //    user.findOne({
+        //        role: 2097151
+        //    }, function (err, doc) {
+        //        if(err){
+        //            return done(err);
+        //        }
+        //
+        //        if(!doc){
+        //            console.log('');
+        //            console.log('#########################################################');
+        //            console.log('miss an owner, please read readme.md and use `npm install`');
+        //            console.log('#########################################################');
+        //            console.log('');
+        //            return process.exit();
+        //        }
+        //
+        //        app.locals.$owner = doc.toObject();
+        //        done();
+        //    });
+        //})
         // 异步并行
         .together(function (err) {
             next(err, app);
