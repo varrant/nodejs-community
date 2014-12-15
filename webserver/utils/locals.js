c358279eeb73ea7b8f03dfe96c0e461b64140f17
cloudcome <cloudcome@163.com>
2014-12-15 21:21:01 +0800

    alpha8

--- webserver/utils/locals.js
+++ webserver/utils/locals.js
@@ -15,15 +15,21 @@ exports.$settings = function (app) {
     var types = app.locals.$settings.types;
     var roles = app.locals.$settings.roles;
     var map1 = {};
+    var list1 = [];
     var map2 = {};
+    var list2 = [];
 
     types.forEach(function (type) {
         map1[type.uri] = type;
+        list1.push(type.uri);
     });
     app.locals.$settings.typesMap = map1;
+    app.locals.$settings.typesList = list1;
 
     roles.forEach(function (role) {
         map2[role.name] = role;
+        list2.push(role.name);
     });
     app.locals.$settings.rolesMap = map2;
+    app.locals.$settings.rolesList = list2;
 };
