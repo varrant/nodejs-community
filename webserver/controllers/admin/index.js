9292d4b45f3bbbdc0ffde3b0ed65c278b5318eef
cloudcome <cloudcome@163.com>
2014-12-15 21:29:38 +0800

    alpha8

--- webserver/controllers/admin/index.js
+++ webserver/controllers/admin/index.js
@@ -11,6 +11,7 @@ module.exports = function (app) {
     return {
         main: require('./main.js')(app),
         middleware: require('./middleware.js')(app),
+        object: require('./object.js')(app),
         role: require('./role.js')(app),
         type: require('./type.js')(app)
     };
