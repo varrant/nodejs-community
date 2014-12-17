f81ef9054ad26c59fcf6333e3e75f4e244e748ad
cloudcome <cloudcome@163.com>
2014-12-17 20:26:31 +0800

    alpha11

--- test/services/object.js
+++ test/services/object.js
@@ -10,45 +10,48 @@ var random = require('ydr-util').random;
 var test = require('../test.js');
 var object = require('../../webserver/services/').object;
 var author = {
-    _id: '548c136f274b6f0000dcf8d9',
+    _id: '5491715834cde400004bf3fc',
     role: 2097151
 };
-var scope1Id = '548c15505bb70f0000bf806e';
-var scope2Id = '548aa07592a2a9dc252266b9';
-var content = 'url:/customer/rest/statistics/chat/custom?platformId=14&customId=25&startTime=1414800000000&endTime=1418256000000&pageNo=1&pageSize=10url:/customer/rest/statistics/chat/custom?platformId=14&customId=25&startTime=1414800000000&endTime=1418256000000&pageNo=1&pageSize=10url:/customer/rest/statistics/chat/custom?platformId=14&customId=25&startTime=1414800000000&endTime=1418256000000&pageNo=1&pageSize=10';
+var scope1Id = '5491729fb70d9d0000f8d363';
+var content = '# 测试数据\n\n需要用什么系统，怎么安装呢需要用什么系统，怎么安装呢需要用什么系统，怎么安装呢需要用什么系统，怎么安装呢';
+var howdo = require('howdo');
+var arr = new Array(100);
 
 test
     .push('object.createOne', function (next) {
-        object.createOne(author, {
-            scope: scope1Id,
-            title: '世界，你好',
-            type: 'help',
-            uri: 'hello-world',
-            content: content,
-            labels: ['label1', 'label2', 'label3']
-        }, function () {
-            console.log(arguments);
-            next();
-        });
+        howdo
+            .each(arr, function (index, val, done) {
+                object.createOne(author, {
+                    scope: scope1Id,
+                    title: '世界，你好 ' + random.string(20),
+                    type: 'question',
+                    uri: 'hello-world-' + random.string(20),
+                    content: content,
+                    labels: [random.string(6),random.string(6),random.string(6),random.string(6),random.string(6)]
+                }, done);
+            })
+            .together(next);
+
     })
-    .push('object.updateOne', function (next) {
-        object.updateOne(author, {
-            type: 'help',
-            uri: 'hello-world'
-        }, {
-            scope: scope2Id,
-            title: '世界，你好',
-            content: content,
-            labels: ['label6']
-        }, function () {
-            console.log(arguments);
-            next();
-        });
-    })
-    .push('object.findOne', function (next) {
-        object.findOne({uri: 'hello-world'}, function (err, doc) {
-            console.log(doc);
-            next();
-        });
-    })
+    //.push('object.updateOne', function (next) {
+    //    object.updateOne(author, {
+    //        type: 'help',
+    //        uri: 'hello-world'
+    //    }, {
+    //        scope: scope2Id,
+    //        title: '世界，你好',
+    //        content: content,
+    //        labels: ['label6']
+    //    }, function () {
+    //        console.log(arguments);
+    //        next();
+    //    });
+    //})
+    //.push('object.findOne', function (next) {
+    //    object.findOne({uri: 'hello-world'}, function (err, doc) {
+    //        console.log(doc);
+    //        next();
+    //    });
+    //})
     .start();
