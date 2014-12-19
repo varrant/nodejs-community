b3ce0d16eb6644a24d260f42383455d7c29b2a98
cloudcome <cloudcome@163.com>
2014-12-19 14:56:18 +0800

    alpha13

--- webroot-dev/static/js/app/admin/object-list.js
+++ webroot-dev/static/js/app/admin/object-list.js
@@ -8,78 +8,12 @@
 define(function (require, exports, module) {
     'use strict';
 
-
-    var ajax = require('../../widget/common/ajax.js');
-    var alert = require('../../widget/common/alert.js');
-    var confirm = require('../../widget/common/confirm.js');
-    var hashbang = require('../../alien/core/navigator/hashbang.js');
-    var qs = require('../../alien/util/querystring.js');
-    var Pagination = require('../../alien/ui/Pagination/index.js');
-    var attribute = require('../../alien/core/dom/attribute.js');
-    var url = '/admin/api/object/list/?type=' + window['-type-'] + '&';
-    var page = {
-        req: {
-            page: hashbang.get('query', 'page') || 1,
-            limit: 20
-        }
-    };
+    var List = require('../../widget/admin/List.js');
 
     require('../../widget/admin/welcome.js');
-
-    hashbang.on('query', 'page', function (eve, neo) {
-        page.req.page = neo.query.page || 1;
-        page.req.limit = neo.query.limit || 20;
-        page.list();
+    new List('#list', '#pagination', {
+        url: '/admin/api/object/list/?type=question&'
     });
 
-    /**
-     * 请求展示列表
-     */
-    page.list = function () {
-        ajax({
-            url: url + qs.stringify(page.req)
-        }).on('success', page.onsuccess).on('error', alert);
-    };
 
-    /**
-     * 请求成功
-     * @param json
-     * @returns {*}
-     */
-    page.onsuccess = function (json) {
-        if (json.code !== 200) {
-            return alert(json);
-        }
-
-        if (page.vue) {
-            page.vue.$data.objects = json.data;
-            page.pagination.render({
-                page: page.req.page,
-                max: Math.ceil(json.count / page.req.limit)
-            });
-        } else {
-            page.vue = new Vue({
-                el: '#list',
-                data: {
-                    objects: json.data,
-                    req: page.req
-                },
-                methods: {}
-            });
-
-            page.vue.$el.classList.remove('f-none');
-            page.pagination = new Pagination('#Pagination', {
-                page: page.req.page,
-                max: Math.ceil(json.count / page.req.limit)
-            }).on('change', function (_page) {
-                    hashbang.set('query', {
-                        page: _page,
-                        limit: page.req.limit
-                    });
-                    attribute.scrollTop(window, 0);
-                });
-        }
-    };
-
-    page.list();
 });
