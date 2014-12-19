bcb4f000a8886a82c111a0705487f9ce74edea8f
cloudcome <cloudcome@163.com>
2014-12-19 15:08:05 +0800

    alpha13

---
+++ webroot-dev/static/js/widget/admin/list.js
@@ -0,0 +1,15 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2014-12-19 15:04
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/List
+     */
+    'use strict';
+
+    module.exports = {};
+});
