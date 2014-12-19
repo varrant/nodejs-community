95cd9c64072694ec9077980db4f1ac4251c68efb
cloudcome <cloudcome@163.com>
2014-12-19 16:51:27 +0800

    alpha13

---
+++ webroot-dev/static/js/app/admin/object-item.js
@@ -0,0 +1,16 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2014-12-19 16:26
+ */
+
+
+define(function (require, exports, module) {
+    'use strict';
+
+    var Item = require('../../ui/admin/Item/');
+
+    new Item('#form', '#content', {
+        id: window['-id-']
+    });
+});
