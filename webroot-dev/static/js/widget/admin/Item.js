31351a425c3ea93313cdcd1a96f464706199143d
cloudcome <cloudcome@163.com>
2014-12-19 14:58:34 +0800

    alpha13

---
+++ webroot-dev/static/js/widget/admin/Item.js
@@ -0,0 +1,17 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2014-12-19 14:56
+ */
+
+
+define(function (require, exports, module) {
+    /**
+     * @module parent/Item
+     */
+    'use strict';
+
+
+
+    module.exports = {};
+});
