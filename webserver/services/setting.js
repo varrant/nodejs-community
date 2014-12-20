9cdc169e183c58c892b9fb722b2186d5e58e365c
cloudcome <cloudcome@163.com>
2014-12-20 23:07:25 +0800

    alpha14

--- webserver/services/setting.js
+++ webserver/services/setting.js
@@ -31,7 +31,6 @@ exports.set = function (key, val, callback) {
         });
     }
 
-    console.log(map);
     howdo.each(map, function (index, json, done) {
         setting.existOne({
             key: json.key
