c1a6ca2b1a94c9fbf8c024bd73bbad79732275e1
cloudcome <cloudcome@163.com>
2014-12-22 18:57:55 +0800

    alpha16

--- webserver/routers/front.js
+++ webserver/routers/front.js
@@ -22,16 +22,72 @@ module.exports = function (app, ctrlFront) {
 
 
     // list + detail
+    // in category at column on label
     section.forEach(function (uri) {
+        // ''
         app.get('/' + uri + '/', ctrlFront.main.getList(uri));
         app.get('/' + uri + '/' + 'page/:page/', ctrlFront.main.getList(uri));
-        app.get('/' + uri + '/' + 'in/:scope/', ctrlFront.main.getList(uri));
-        app.get('/' + uri + '/' + 'in/:scope/page/:page/', ctrlFront.main.getList(uri));
+
+        // category
+        app.get('/' + uri + '/' + 'in/:category/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'in/:category/page/:page/', ctrlFront.main.getList(uri));
+
+        // column
+        app.get('/' + uri + '/' + 'at/:column/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'at/:column/page/:page/', ctrlFront.main.getList(uri));
+
+        // label
+        app.get('/' + uri + '/' + 'on/:label/', ctrlFront.main.getList(uri));
         app.get('/' + uri + '/' + 'on/:label/page/:page/', ctrlFront.main.getList(uri));
-        app.get('/' + uri + '/' + 'in/:scope/on/:label/', ctrlFront.main.getList(uri));
-        app.get('/' + uri + '/' + 'in/:scope/on/:label/page/:page/', ctrlFront.main.getList(uri));
-        app.get('/' + uri + '/' + 'on/:label/in/:scope/', ctrlFront.main.getList(uri));
-        app.get('/' + uri + '/' + 'on/:label/in/:scope/page/:page/', ctrlFront.main.getList(uri));
+
+        // category + column
+        app.get('/' + uri + '/' + 'in/:category/at/:column/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/page/:page/', ctrlFront.main.getList(uri));
+
+        // category + label
+        app.get('/' + uri + '/' + 'in/:category/on/:label/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'in/:category/on/:label/page/:page/', ctrlFront.main.getList(uri));
+
+        // column + category
+        app.get('/' + uri + '/' + 'at/:column/in/:category/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'at/:column/in/:category/page/:page/', ctrlFront.main.getList(uri));
+
+        // column + label
+        app.get('/' + uri + '/' + 'at/:column/on/:label/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'at/:column/on/:label/page/:page/', ctrlFront.main.getList(uri));
+
+        // label + column
+        app.get('/' + uri + '/' + 'on/:label/at/:column/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'on/:label/at/:column/page/:page/', ctrlFront.main.getList(uri));
+
+        // label + category
+        app.get('/' + uri + '/' + 'on/:label/in/:category/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'on/:label/in/:category/page/:page/', ctrlFront.main.getList(uri));
+
+        // category + column + label
+        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/page/:page/', ctrlFront.main.getList(uri));
+
+        // category + label + column
+        app.get('/' + uri + '/' + 'in/:category/on/:label/at/:column/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'in/:category/on/:label/at/:column/page/:page/', ctrlFront.main.getList(uri));
+
+        // column + category + label
+        app.get('/' + uri + '/' + 'at/:column/in/:category/on/:label/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'at/:column/in/:category/on/:label/page/:page/', ctrlFront.main.getList(uri));
+
+        // column + label + category
+        app.get('/' + uri + '/' + 'at/:column/on/:label/in/:category/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'at/:column/on/:label/in/:category/page/:page/', ctrlFront.main.getList(uri));
+
+        // label + category + column
+        app.get('/' + uri + '/' + 'on/:label/in/:category/at/:column/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'on/:label/in/:category/at/:column/page/:page/', ctrlFront.main.getList(uri));
+
+        // label + column + category
+        app.get('/' + uri + '/' + 'on/:label/at/:column/in/:category/', ctrlFront.main.getList(uri));
+        app.get('/' + uri + '/' + 'on/:label/at/:column/in/:category/page/:page/', ctrlFront.main.getList(uri));
+
         app.get('/' + uri + '/' + ':uri.html', ctrlFront.main.getPost(uri));
     });
 
