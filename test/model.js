57f30826d144953cadb2cb413aab3fba10401b83
cloudcome <cloudcome@163.com>
2014-12-22 18:34:56 +0800

    alpha16

--- test/model.js
+++ test/model.js
@@ -5,10 +5,7 @@
  */
 
 
-var mongoose = require('../webserver/mongoose.js');
 var engineer = require('../webserver/models/').engineer;
-var object = require('../webserver/models/').object;
-var scope = require('../webserver/models/').scope;
 var random = require('ydr-util').random;
 var test = require('./test.js');
 
