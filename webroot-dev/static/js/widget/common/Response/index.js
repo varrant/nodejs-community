b5a7942408c53d35e25bf295ea10e9316a7e6062
cloudcome <cloudcome@163.com>
2014-12-28 20:37:45 +0800

    alpha25

--- webroot-dev/static/js/widget/common/Response/index.js
+++ webroot-dev/static/js/widget/common/Response/index.js
@@ -647,14 +647,16 @@ define(function (require, exports, module) {
                     if (method === 'delete') {
                         the._acceptItem(options.acceptByResponse, false);
                         options.acceptByResponse = '';
+                        options.list.object.acceptByResponse = '';
                         the.emit('accept', false);
                     }
                     // 设置最佳
                     else {
                         options.acceptByResponse = id;
+                        options.list.object.acceptByResponse = id;
                         the._acceptItem(id, true);
 
-                        if (id !== json.data) {
+                        if (json.data) {
                             the._acceptItem(json.data, false);
                         }
 
