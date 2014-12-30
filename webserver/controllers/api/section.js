46d5fd03ee73e3a712e853f88bf4ec14d1bb04d5
cloudcome <cloudcome@163.com>
2014-12-30 11:26:26 +0800

    alpha27

--- webserver/controllers/api/section.js
+++ webserver/controllers/api/section.js
@@ -20,15 +20,22 @@ module.exports = function (app) {
      * @param next
      */
     exports.get = function (req, res, next) {
-        if(!permission.can(res.locals.$developer, 'section')){
+        if (!permission.can(res.locals.$developer, 'section')) {
             var err = new Error('权限不足');
             err.code = 403;
             return next(err);
         }
 
-        res.json({
-            code: 200,
-            data: app.locals.$section
+        section.find({}, function (err, docs) {
+            if (err) {
+                return next(err);
+            }
+
+            res.json({
+                code: 200,
+                data: docs
+            });
+            app.locals.$section = docs;
         });
     };
 
@@ -40,7 +47,7 @@ module.exports = function (app) {
      * @param next
      */
     exports.put = function (req, res, next) {
-        if(!permission.can(res.locals.$developer, 'section')){
+        if (!permission.can(res.locals.$developer, 'section')) {
             var err = new Error('权限不足');
             err.code = 403;
             return next(err);
@@ -56,8 +63,8 @@ module.exports = function (app) {
                     return next(err);
                 }
 
-                dato.each(app.locals.$section, function (index, section) {
-                    if (section.id.toString() === doc.id.toString()) {
+                dato.each(app.locals.$section, function (index, sec) {
+                    if (sec.id.toString() === doc.id.toString()) {
                         app.locals.$section[index] = doc;
                         return false;
                     }
@@ -91,7 +98,7 @@ module.exports = function (app) {
      * @param next
      */
     exports.delete = function (req, res, next) {
-        if(!permission.can(res.locals.$developer, 'section')){
+        if (!permission.can(res.locals.$developer, 'section')) {
             var err = new Error('权限不足');
             err.code = 403;
             return next(err);
