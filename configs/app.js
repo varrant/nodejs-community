94f665c0257d35ecf1ddeb68be9802dec9ca1ccd
cloudcome <cloudcome@163.com>
2014-12-31 15:00:30 +0800

    alpha29

--- configs/app.js
+++ configs/app.js
@@ -6,14 +6,15 @@
 
 'use strict';
 
+var isAliYun = process.env && process.env.HOSTNAME === 'AY140601002820618982Z';
 
 module.exports = {
     // 可选环境: dev/pro
-    env: process.env === 'AY140601002820618982Z' ? 'pro' : 'dev',
+    env: isAliYun ? 'pro' : 'dev',
     // 端口
     port: 18084,
     // 主机
-    host: 'http://sb.com:18084',
+    host: isAliYun ? 'http://112.124.114.165:18084' : 'http://sb.com:18084',
     // mongodb
     mongodb: 'mongodb://localhost:27017/community'
 };
