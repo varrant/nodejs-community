a16dc0d6220df3c88c5f31d08a4507d1dfc7f805
cloudcome <cloudcome@163.com>
2015-01-21 20:34:03 +0800

    beta 4

--- configs/score.js
+++ configs/score.js
@@ -8,6 +8,11 @@
 
 
 module.exports = function (app) {
+    // 以下都是固定值
+    // 被动威望的增长为：dato.parseInt(固定值 + Math.round(Math.log10(主动人的当前威望值 + 被动人的当前威望值)), 1);
+    // 即：如果A（100威望）给B（200威望）的 object 评论。
+    // 主动人增加的威望 = 2
+    // 被动人增加的威望 = 1 + Math.log10(100 + 200)
     return {
         // 发布帮助
         help: 1,
---
+++ webserver/utils/score.js
@@ -0,0 +1,78 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-01-21 20:26
+ */
+
+'use strict';
+
+var dato = require('ydr-util').dato;
+var configs = require('../../configs/');
+var scoreMap = configs.score;
+
+
+/**
+ * 被动评论分
+ * @param sourceDevloper
+ * @param targetDevloper
+ * @returns {*}
+ */
+exports.commentBy = function (sourceDevloper, targetDevloper) {
+    return _increase(scoreMap.commentBy, sourceDevloper, targetDevloper);
+};
+
+
+/**
+ * 被动评论分
+ * @param sourceDevloper
+ * @param targetDevloper
+ * @returns {*}
+ */
+exports.replyBy = function (sourceDevloper, targetDevloper) {
+    return _increase(scoreMap.replyBy, sourceDevloper, targetDevloper);
+};
+
+
+/**
+ * 被动评论分
+ * @param sourceDevloper
+ * @param targetDevloper
+ * @returns {*}
+ */
+exports.acceptBy = function (sourceDevloper, targetDevloper) {
+    return _increase(scoreMap.acceptBy, sourceDevloper, targetDevloper);
+};
+
+
+/**
+ * 被动评论分
+ * @param sourceDevloper
+ * @param targetDevloper
+ * @returns {*}
+ */
+exports.agreeBy = function (sourceDevloper, targetDevloper) {
+    return _increase(scoreMap.agreeBy, sourceDevloper, targetDevloper);
+};
+
+
+/**
+ * 增加的分值
+ * @param S
+ * @param A
+ * @param B
+ * @returns {*}
+ * @private
+ */
+function _increase(S, A, B) {
+    return dato.parseInt(Math.round(S + log10(A.score + B.score)), S);
+}
+
+
+/**
+ * Math.log10(x);
+ * @param x
+ * @returns {number}
+ */
+function log10(x) {
+    return Math.log(x) / Math.LN10;
+}
