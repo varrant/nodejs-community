c5591d697bcb1e5354eaa367d824e3c755728b71
cloudcome <cloudcome@163.com>
2015-02-04 22:24:46 +0800

    beta12

--- cli/interactive-model.js
+++ cli/interactive-model.js
@@ -20,11 +20,7 @@ mongoose(function (err) {
         return process.exit();
     }
 
-    response.find({}, {
-        nor: {
-            parent: null
-        }
-    }, function (err, docs) {
+    interactive.find({}, function (err, docs) {
         if (err) {
             console.log('query interactive error');
             console.error(err);
@@ -35,20 +31,47 @@ mongoose(function (err) {
             .each(docs, function (index, doc, next) {
                 console.log('update ' + doc.id.toString() + ' now');
 
-                response.findOneAndUpdate({
-                    _id: doc.id
-                }, {
-                    parentResponse: parentResponse.id.toString(),
-                    parentAuthor: parentResponse.author.toString()
-                }, next);
+                howdo
+                    // 查找评论
+                    .task(function (next) {
+                        response.findOne({
+                            _id: doc.response
+                        }, function (err, res) {
+                            if (err) {
+                                console.log('find response error');
+                                return next(err);
+                            }
 
+                            if (!res) {
+                                err = new Error('response ' + doc.response + ' not found');
+                                return next();
+                            }
+
+                            next(err, res);
+                        });
+                    })
+                    // 修正
+                    .task(function (next, res) {
+                        if (!res) {
+                            return next();
+                        }
+
+                        interactive.findOneAndUpdate({
+                            _id: doc.id
+                        }, {
+                            source: doc.operator,
+                            target: res.author
+                        }, next);
+                    })
+                    // 异步串行
+                    .follow(next);
             })
             .follow(function (err) {
                 if (err) {
-                    console.log('update response error');
+                    console.log('update interactive error');
                     console.error(err);
                 } else {
-                    console.log('update response success');
+                    console.log('update interactive success');
                 }
 
                 process.exit();
