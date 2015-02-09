f1387d7a20d433b3199c87b7dfeb57aa1edde367
cloudcome <cloudcome@163.com>
2015-02-09 21:17:20 +0800

    beta16

--- webserver/controllers/admin/developer.js
+++ webserver/controllers/admin/developer.js
@@ -14,49 +14,6 @@ module.exports = function (app) {
     var exports = {};
 
     /**
-     * 用户列表
-     * @param req
-     * @param res
-     * @param next
-     */
-    exports.list = function (req, res, next) {
-        var data = {
-            title: '用户管理'
-        };
-
-        var engineerRole = res.locals.$developer.role;
-
-        if ((engineerRole & role20) === 0) {
-            return next();
-        }
-
-        res.render('admin/developer-list.html', data);
-    };
-
-
-    /**
-     * 获取某个用户
-     * @param req
-     * @param res
-     * @param next
-     */
-    exports.get = function (req, res, next) {
-        var data = {
-            title: '用户详情',
-            id: req.query.id || ''
-        };
-
-        var engineerRole = res.locals.$developer.role;
-
-        if ((engineerRole & role20) === 0) {
-            return next();
-        }
-
-        res.render('admin/developer-detail.html', data);
-    };
-
-
-    /**
      * 我
      * @param req
      * @param res
