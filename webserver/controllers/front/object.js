c2e63875675d6d9d97446ce890b282f54ac024d1
cloudcome <cloudcome@163.com>
2015-02-12 21:11:14 +0800

    beta18

--- webserver/controllers/front/object.js
+++ webserver/controllers/front/object.js
@@ -6,6 +6,213 @@
 
 'use strict';
 
-var fs = require('fs');
 
-module.exports = {};
+var random = require('ydr-util').random;
+var dato = require('ydr-util').dato;
+var typeis = require('ydr-util').typeis;
+var howdo = require('howdo');
+var object = require('../../services/').object;
+var developer = require('../../services/').developer;
+var column = require('../../services/').column;
+var section = require('../../services/').section;
+var filter = require('../../utils/').filter;
+var log = require('ydr-log');
+
+module.exports = function (app) {
+    var exports = {};
+
+    /**
+     * list 页
+     */
+    exports.getList = function (section) {
+        return function (req, res, next) {
+            var category = req.params.category;
+            var columnId = req.params.column;
+            var label = req.params.label;
+            var status = req.params.status;
+            var author = req.params.author;
+            var listOptions = filter.skipLimit(req.params);
+            var conditions = {
+                section: section.id,
+                isDisplay: true
+            };
+            var categoryMap = {};
+            var data = {
+                section: section,
+                title: section.name,
+                categories: app.locals.$category,
+                categoryMap: categoryMap,
+                choose: {}
+            };
+            //var isPjax = req.headers['x-request-as'] === 'pjax';
+            var categoryId = 0;
+            dato.each(app.locals.$category, function (index, _category) {
+                if (_category.uri === category) {
+                    categoryId = _category.id;
+                }
+
+                categoryMap[_category.id] = _category;
+            });
+
+            if (categoryId) {
+                data.choose.category = conditions.category = categoryId;
+            }
+
+            if (label) {
+                data.choose.label = conditions.labels = label;
+            }
+
+
+            var countOptions = {};
+            if (status === 'resolved') {
+                data.choose.status = 'resolved';
+                listOptions.nor = {acceptByResponse: null};
+                countOptions.nor = {acceptByResponse: null};
+            } else if (status === 'unresolved') {
+                data.choose.status = 'unresolved';
+                conditions.acceptByResponse = null;
+            }
+
+            var onnext = function () {
+                howdo
+                    // 统计数量
+                    .task(function (done) {
+                        object.count(conditions, countOptions, done);
+                    })
+                    // 列表
+                    .task(function (done) {
+                        listOptions.populate = ['author', 'contributors'];
+                        listOptions.sort = {publishAt: -1};
+                        object.find(conditions, listOptions, done);
+                    })
+                    // 查找 columns
+                    .task(function (done) {
+                        column.find({
+                            author: res.locals.$developer.id
+                        }, done);
+                    })
+                    // 异步并行
+                    .together(function (err, count, docs, columns) {
+                        if (err) {
+                            return next(err);
+                        }
+
+                        data.columnsMap = {};
+
+                        columns.forEach(function (item) {
+                            data.columnsMap[item.id] = item;
+                        });
+
+                        data.objects = docs;
+                        data.pager = {
+                            page: listOptions.page,
+                            limit: listOptions.limit,
+                            count: count
+                        };
+                        //data.pagination = pagi.pagination({
+                        //    page: listOptions.page,
+                        //    max: Math.ceil(count / 20),
+                        //    url: '/' + section.uri + '/page/:page/'
+                        //});
+                        //if(isPjax){
+                        //    return res.json({
+                        //        code: 200,
+                        //        data: data
+                        //    });
+                        //}
+
+                        res.render('front/list-' + section.uri + '.html', data);
+                    });
+            };
+
+            if (author) {
+                // 查找作者
+                developer.findOne({
+                    githubLogin: author
+                }, function (err, de) {
+                    if (err) {
+                        return next(err);
+                    }
+
+                    if (!de) {
+                        return next();
+                    }
+
+                    data.choose.author = de.githubLogin;
+                    data.choose.authorNickname = de.nickname;
+                    conditions.author = de.id.toString();
+                    onnext();
+                });
+            } else {
+                onnext();
+            }
+        };
+    };
+
+
+    /**
+     * ID 303 => uri
+     * @param req
+     * @param res
+     * @param next
+     */
+    exports.redirect = function (req, res, next) {
+        var id = req.query.id;
+
+        object.findOne({
+            _id: id,
+            isDisplay: true
+        }, function (err, doc) {
+            if (err) {
+                if (err) {
+                    return next(err);
+                }
+
+                if (!doc) {
+                    return next();
+                }
+            }
+
+            var sectionMap = {};
+            app.locals.$section.forEach(function (sec) {
+                sectionMap[sec.id] = sec;
+            });
+
+            res.redirect('/' + sectionMap[doc.section].uri + '/' + doc.uri + '.html');
+        });
+    };
+
+
+    /**
+     * post 页
+     * @param type
+     * @returns {Function}(req, res, next)
+     */
+    exports.getObject = function (section) {
+        return function (req, res, next) {
+            var uri = req.params.uri;
+            var data = {};
+
+            object.findOne({
+                section: section.id,
+                uri: uri,
+                isDisplay: true
+            }, {populate: ['author', 'category']}, function (err, doc) {
+                if (err) {
+                    return next(err);
+                }
+
+                if (!doc) {
+                    return next();
+                }
+
+                object.increaseViewByCount({_id: doc.id}, 1, log.holdError);
+                data.title = doc.title;
+                data.object = doc;
+                res.render('front/object-' + section.uri + '.html', data);
+            });
+        };
+    };
+
+    return exports;
+};
--- webserver/routers/front.js
+++ webserver/routers/front.js
@@ -26,116 +26,116 @@ module.exports = function (app, ctrl) {
         var uri = section.uri;
 
         // ''
-        app.get('/' + uri + '/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'page/:page/', ctrl.object.getList(section));
 
         // category
-        app.get('/' + uri + '/' + 'in/:category/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/page/:page/', ctrl.object.getList(section));
 
         // column
-        app.get('/' + uri + '/' + 'at/:column/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'at/:column/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/page/:page/', ctrl.object.getList(section));
 
         // label
-        app.get('/' + uri + '/' + 'on/:label/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'on/:label/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'on/:label/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'on/:label/page/:page/', ctrl.object.getList(section));
 
         // status
-        app.get('/' + uri + '/' + 'as/:status/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'as/:status/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'as/:status/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'as/:status/page/:page/', ctrl.object.getList(section));
 
         // author
-        app.get('/' + uri + '/' + 'by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'by/:author/page/:page/', ctrl.object.getList(section));
 
         // category + column
-        app.get('/' + uri + '/' + 'in/:category/at/:column/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/at/:column/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/page/:page/', ctrl.object.getList(section));
 
         // category + label
-        app.get('/' + uri + '/' + 'in/:category/on/:label/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/on/:label/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/on/:label/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/on/:label/page/:page/', ctrl.object.getList(section));
 
         // category + status
-        app.get('/' + uri + '/' + 'in/:category/as/:status/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/as/:status/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/as/:status/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/as/:status/page/:page/', ctrl.object.getList(section));
 
         // category + author
-        app.get('/' + uri + '/' + 'in/:category/by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/by/:author/page/:page/', ctrl.object.getList(section));
 
         // column + label
-        app.get('/' + uri + '/' + 'at/:column/on/:label/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'at/:column/on/:label/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/on/:label/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/on/:label/page/:page/', ctrl.object.getList(section));
 
         // column + status
-        app.get('/' + uri + '/' + 'at/:column/as/:status/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'at/:column/as/:status/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/as/:status/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/as/:status/page/:page/', ctrl.object.getList(section));
 
         // column + author
-        app.get('/' + uri + '/' + 'at/:column/by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'at/:column/by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/by/:author/page/:page/', ctrl.object.getList(section));
 
         // label + status
-        app.get('/' + uri + '/' + 'on/:label/as/:status/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'on/:label/as/:status/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'on/:label/as/:status/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'on/:label/as/:status/page/:page/', ctrl.object.getList(section));
 
         // label + author
-        app.get('/' + uri + '/' + 'on/:label/by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'on/:label/by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'on/:label/by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'on/:label/by/:author/page/:page/', ctrl.object.getList(section));
 
         // status + author
-        app.get('/' + uri + '/' + 'as/:status/by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'as/:status/by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'as/:status/by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'as/:status/by/:author/page/:page/', ctrl.object.getList(section));
 
         // category + column + label
-        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/page/:page/', ctrl.object.getList(section));
 
         // category + column + status
-        app.get('/' + uri + '/' + 'in/:category/at/:column/as/:status/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/at/:column/as/:status/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/as/:status/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/as/:status/page/:page/', ctrl.object.getList(section));
 
         // category + column + author
-        app.get('/' + uri + '/' + 'in/:category/at/:column/by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/at/:column/by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/by/:author/page/:page/', ctrl.object.getList(section));
 
         // category + label + status
-        app.get('/' + uri + '/' + 'in/:category/on/:label/as/:status/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/on/:label/as/:status/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/on/:label/as/:status/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/on/:label/as/:status/page/:page/', ctrl.object.getList(section));
 
         // category + label + author
-        app.get('/' + uri + '/' + 'in/:category/on/:label/by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/on/:label/by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/on/:label/by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/on/:label/by/:author/page/:page/', ctrl.object.getList(section));
 
         // category + status + author
-        app.get('/' + uri + '/' + 'in/:category/as/:status/by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/as/:status/by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/as/:status/by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/as/:status/by/:author/page/:page/', ctrl.object.getList(section));
 
         // column + label + status
-        app.get('/' + uri + '/' + 'at/:column/on/:label/as/:status/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'at/:column/on/:label/as/:status/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/on/:label/as/:status/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/on/:label/as/:status/page/:page/', ctrl.object.getList(section));
 
         // column + label + author
-        app.get('/' + uri + '/' + 'at/:column/on/:label/by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'at/:column/on/:label/by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/on/:label/by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'at/:column/on/:label/by/:author/page/:page/', ctrl.object.getList(section));
 
         // label + status + author
-        app.get('/' + uri + '/' + 'on/:label/as/:status/by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'on/:label/as/:status/by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'on/:label/as/:status/by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'on/:label/as/:status/by/:author/page/:page/', ctrl.object.getList(section));
 
         // category + column + label + status
-        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/as/:status/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/as/:status/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/as/:status/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/as/:status/page/:page/', ctrl.object.getList(section));
 
         // category + column + label + author
-        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/by/:author/page/:page/', ctrl.object.getList(section));
 
         // category + column + label + status + author
-        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/as/:status/by/:author/', ctrl.main.getList(section));
-        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/as/:status/by/:author/page/:page/', ctrl.main.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/as/:status/by/:author/', ctrl.object.getList(section));
+        app.get('/' + uri + '/' + 'in/:category/at/:column/on/:label/as/:status/by/:author/page/:page/', ctrl.object.getList(section));
 
         app.get('/object/', ctrl.main.redirect);
         app.get('/' + uri + '/' + ':uri.html', ctrl.main.getObject(section));
