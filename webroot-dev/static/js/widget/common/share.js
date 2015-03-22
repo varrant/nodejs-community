ba73f6fe5f5dbb83b3a9034986439b8f77b4528f
cloudcome <cloudcome@163.com>
2015-03-22 16:26:41 +0800

    0.0.4

---
+++ webroot-dev/static/js/widget/common/share.js
@@ -0,0 +1,27 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-03-22 16:24
+ */
+
+
+define(function (require, exports, module) {
+    'use strict';
+
+    window._bd_share_config = {
+        "common": {
+            "bdSnsKey": {},
+            "bdText": "",
+            "bdMini": "2",
+            "bdMiniList": false,
+            "bdPic": "",
+            "bdStyle": "1",
+            "bdSize": "16"
+        },
+        "share": {}
+    };
+
+    var src = 'http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion=' + ~(-new Date() / 36e5);
+
+
+});
