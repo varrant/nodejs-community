044677b6d4f0101e6fab509d8825fd49fd6a0ae9
cloudcome <cloudcome@163.com>
2015-04-10 01:21:59 +0800

    ydr-util => ydr-utils

--- webroot-dev/static/js/widget/common/alert.js
+++ webroot-dev/static/js/widget/common/alert.js
@@ -9,13 +9,19 @@ define(function (require, exports, module) {
     'use strict';
 
     var alert = require('../../alien/widgets/alert.js');
+    var modification = require('../../alien/core/dom/modification.js');
     var login = require('./login.js');
 
     module.exports = function (content) {
+        var $div = modification.create('div');
+
+        $div.innerHTML = content && content.message ? content : String(content);
+        content = $div.innerText;
+
         return alert(content).on('sure', function () {
-                if(content && content.code === 401){
-                    login();
-                }
-            });
+            if (content && content.code === 401) {
+                login();
+            }
+        });
     };
 });
