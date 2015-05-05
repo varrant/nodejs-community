f08b45a84e0656343f051ea8ec1c978781a136b5
张云来 <cloudcome@163.com>
2015-05-06 01:28:29 +0800

    list

--- webroot-dev/static/js/app/front/home.js
+++ webroot-dev/static/js/app/front/home.js
@@ -11,4 +11,18 @@ define(function (require, exports, module) {
     require('../../widget/front/login.js');
     require('../../widget/front/nav.js');
     require('../../widget/front/footer.js');
+
+    var keyframes = require('../../alien/core/dom/keyframes.js');
+    var selector = require('../../alien/core/dom/selector.js');
+    var animation = require('../../alien/core/dom/animation.js');
+    var app = {};
+
+    app.list = function () {
+        var $list = selector.query('#list')[0];
+        var $items = selector.query('li', $list);
+
+
+    };
+
+    app.list();
 });
