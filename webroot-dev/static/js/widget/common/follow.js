064a38feaf793672b40ee17b801814b341aa1010
张云来 <cloudcome@163.com>
2015-05-17 19:48:36 +0800

    follow

---
+++ webroot-dev/static/js/widget/common/follow.js
@@ -0,0 +1,16 @@
+/*!
+ * 关注
+ * @author ydr.me
+ * @create 2015-05-17 19:44
+ */
+
+
+define(function (require, exports, module) {
+    'use strict';
+
+    var selector = require('../../alien/core/dom/selector.js');
+    var attribute = require('../../alien/core/dom/attribute.js');
+    var event = require('../../alien/core/event/base.js');
+
+
+});
