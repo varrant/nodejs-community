05de1d28c43aba8732de62250e0ad1837fafdb97
张云来 <cloudcome@163.com>
2015-05-25 00:27:06 +0800

    visitors

--- webserver/services/developer.js
+++ webserver/services/developer.js
@@ -912,7 +912,8 @@ exports.oauthCallback = function (oauthSettings, code, callback) {
 exports.pushVisitor = function (conditions, visitor, callback) {
     developer.push(conditions, 'visitors', visitor.id.toString(), {
         unique: true,
-        maxLength: 20
+        maxLength: 20,
+        isDeleteDuplicatorAndPush: true
     }, callback);
 };
 
