58191cb0df8af0d000991ba3795e186a31639c5b
张云来 <cloudcome@163.com>
2015-06-23 20:20:48 +0800

    f: flat response

---
+++ clis/0.3.22/0-flag-response.js
@@ -0,0 +1,78 @@
+/*!
+ * 文件描述
+ * @author ydr.me
+ * @create 2015-06-23 19:50
+ */
+
+
+'use strict';
+
+
+var mongoose = require('../../webserver/mongoose.js');
+var developer = require('../../webserver/services/').developer;
+var response = require('../../webserver/services/').response;
+var howdo = require('howdo');
+
+
+mongoose(function (err) {
+    if (err) {
+        console.log('connect mongodb error');
+        console.error(err.stack);
+        return process.exit();
+    }
+
+
+    response.find({}, {
+        nor: {
+            parentResponse: null
+        }
+    }, function (err, list) {
+        if (err) {
+            console.log('find interactive error');
+            console.error(err.stack);
+            return process.exit();
+        }
+
+        console.log(list.length);
+        howdo.each(list, function (index, resp, next1) {
+            console.log('do', resp.id);
+
+            howdo
+                // 查找父级
+                .task(function (next2) {
+                    response.findOne({
+                        _id: resp.parentResponse
+                    }, next2);
+                })
+                // 查找父级
+                .task(function (next2, parent) {
+                    developer.findOne({
+                        _id: parent.author
+                    }, next2);
+                })
+                // 修改评论内容
+                .task(function (next2, author) {
+                    resp.atList = resp.atList || [];
+                    resp.atList.push(author.id.toString());
+                    response.findOneAndUpdate({
+                        _id: resp.id.toString()
+                    }, {
+                        content: '@' + author.githubLogin + ' ' + resp.content,
+                        contentHTML: '<a href="/developer/' + author.githubLogin + '" class="at">@' + author.githubLogin + '</a> ' + resp.contentHTML,
+                        parentAuthor: null,
+                        parentResponse: null,
+                        atList: resp.atList
+                    }, next2);
+                })
+                .follow(next1);
+        }).follow(function (err) {
+            if (err) {
+                console.log(err.stack);
+                return process.exit();
+            }
+
+            console.log('do success');
+            return process.exit();
+        });
+    });
+});
