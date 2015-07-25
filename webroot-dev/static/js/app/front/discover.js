7327452ff1ab8679be78823d6d5d3eb83963541f
张云来 <cloudcome@163.com>
2015-07-25 23:30:07 +0800

    discover

--- webroot-dev/static/js/app/front/discover.js
+++ webroot-dev/static/js/app/front/discover.js
@@ -11,4 +11,28 @@ define(function (require, exports, module) {
     require('../../modules/front/login.js');
     require('../../modules/front/nav.js');
     require('../../modules/front/footer.js');
+
+    var selector = require('../../alien/core/dom/selector.js');
+    var attribute = require('../../alien/core/dom/attribute.js');
+    var event = require('../../alien/core/event/touch.js');
+    var number = require('../../alien/utils/number.js');
+    var ajax = require('../../modules/common/ajax.js');
+    var app = {};
+
+    app.init = function () {
+        var $list = selector.query('#list')[0];
+
+        event.on($list, 'click', 'a', function () {
+            var $views = selector.query('.links-views', this)[0];
+            var now = number.parseInt($views.innerHTML, 1);
+
+            $views.innerHTML = ++now;
+
+            var img = new Image();
+
+            img.src = '/api/link/views/?id=' + attribute.data($views, 'id');
+        });
+    };
+
+    app.init();
 });
--- webserver/controllers/front/discover.js
+++ webserver/controllers/front/discover.js
@@ -14,98 +14,6 @@ module.exports = function (app) {
 
     // 发现首页
     exports.getHome = function (req, res, next) {
-        var list = [];
-        var listCommunities = [];
-        var listGroups = [];
-        var listBlogs = [];
-
-        list.push({
-            type: '社区',
-            list: listCommunities
-        });
-        listCommunities.push({
-            link: 'http://frontenddev.org/',
-            text: 'FED社区'
-        });
-        listCommunities.push({
-            link: 'http://segmentfault.com/',
-            text: 'SegmentFault'
-        });
-        listCommunities.push({
-            link: 'https://www.v2ex.com/',
-            text: 'V2EX'
-        });
-        listCommunities.push({
-            link: 'http://www.oschina.net/',
-            text: '开源中国'
-        });
-        listCommunities.push({
-            link: 'http://div.io/',
-            text: 'Div.IO'
-        });
-        listCommunities.push({
-            link: 'http://www.html-js.com/',
-            text: '前端乱炖'
-        });
-        listCommunities.push({
-            link: 'https://cnodejs.org/',
-            text: 'CNode'
-        });
-        listCommunities.push({
-            link: 'http://f2e.im/',
-            text: 'F2E社区'
-        });
-        listCommunities.push({
-            link: 'http://angularjs.cn/',
-            text: 'AngularJS中文社区'
-        });
-        listCommunities.push({
-            link: 'http://ionichina.com/',
-            text: 'Ionic 中文社区'
-        });
-
-        list.push({
-            type: '团队',
-            list: listGroups
-        });
-        listGroups.push({
-            link: 'http://ued.taobao.org/blog/',
-            text: '淘宝UED'
-        });
-        listGroups.push({
-            link: 'http://ued.tmall.com/',
-            text: '天猫UED'
-        });
-        listGroups.push({
-            link: 'http://ux.etao.com/',
-            text: '一淘UED'
-        });
-        listGroups.push({
-            link: 'http://tgideas.qq.com/',
-            text: '腾讯TGideas'
-        });
-        listGroups.push({
-            link: 'http://isux.tencent.com/',
-            text: '腾讯ISUX'
-        });
-        listGroups.push({
-            link: 'http://cdc.tencent.com/',
-            text: '腾讯CDC'
-        });
-
-        list.push({
-            type: '博客',
-            list: listBlogs
-        });
-        listBlogs.push({
-            link: 'http://qianduanblog.com/',
-            text: '前端博客'
-        });
-        listBlogs.push({
-            link: 'http://www.zhangxinxu.com/wordpress/',
-            text: '张鑫旭博客'
-        });
-
         var data = {
             title: '发现',
             keywords: '前端，你的世界，等你去发现',
@@ -115,6 +23,7 @@ module.exports = function (app) {
 
         res.render('front/discover.html', data);
     };
+
 
     return exports;
 };
