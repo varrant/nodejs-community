daa266b58e34675a9f1de1426f1e4124a0d19f8e
张云来 <cloudcome@163.com>
2015-07-25 20:50:46 +0800

    discover

---
+++ webroot-dev/static/js/app/front/discover.js
@@ -0,0 +1,14 @@
+/*!
+ * dicover
+ * @author ydr.me
+ * @create 2014-12-13 23:25
+ */
+
+
+define(function (require, exports, module) {
+    'use strict';
+
+    require('../../modules/front/login.js');
+    require('../../modules/front/nav.js');
+    require('../../modules/front/footer.js');
+});
